4cc40b4c11e7fdda93b495aa75149ef3be24412d
Alex <alex_ha@live.de>
2020-05-31 06:58:51 +0200

    enable algorithm switching

--- screenshotmatcher/gui/tray.py
+++ screenshotmatcher/gui/tray.py
@@ -4,25 +4,56 @@ import os
 import subprocess
 import platform
 import common.utils
+from common.config import Config
 
 class Tray():
 
   def __init__(self, app_name):
     self.app_name = app_name
+
+    self.ALGORITHM_STATE = Config.CURRENT_ALGORITHM
+
     self.icon = Icon(
         self.app_name,
         icon=self.get_icon(),
         menu=menu(
             item(
+                'Matching Algorithm',
+                lambda icon: None,
+                enabled=False),
+            item(
+                'SURF (Precise)',
+                self.set_state('SURF'),
+                checked=self.get_state('SURF'),
+                radio=True),
+            item(
+                'ORB (Fast)',
+                self.set_state('ORB'),
+                checked=self.get_state('ORB'),
+                radio=True),
+            menu.SEPARATOR,
+            item(
                 'Show QR code',
                 lambda icon: self.onclick_qr()),
             item(
-                'Show Results',
+                'Open Results Dir',
                 lambda icon: self.onclick_results()),
             menu.SEPARATOR,
             item(
                 'Quit',
                 lambda icon: self.onclick_quit())))
+  
+  def set_state(self, v):
+    def inner(icon, item):
+      self.ALGORITHM_STATE = v
+      print('Switching Algorithm to %s' % self.ALGORITHM_STATE)
+      Config.CURRENT_ALGORITHM = self.ALGORITHM_STATE
+    return inner
+
+  def get_state(self, v):
+    def inner(item):
+      return self.ALGORITHM_STATE == v
+    return inner
 
   def get_icon(self):
     width = 64
@@ -40,6 +71,16 @@ class Tray():
 
   def onclick_quit(self):
     self.icon.stop()
+
+  def onclick_test(self):
+    print(Config.CURRENT_ALGORITHM)
+    print(self.testvar)
+
+  def onclick_switch_algo(self):
+    Config.CURRENT_ALGORITHM = ':-)'
+    self.testvar = ':-)'
+    self.icon.stop()
+    self.icon.run(self.setup)
 
   def onclick_qr(self):
     if platform.system() == 'Linux':
