ded5d749540376c335c802d3dfa8fa6b10235809
Alex <alex_ha@live.de>
2020-06-06 05:50:46 +0200

    identifier generation and storage

--- screenshotmatcher/common/config.py
+++ screenshotmatcher/common/config.py
@@ -1,8 +1,30 @@
 import common.utils
 import sys
+import hashlib
+import platform
+import random
+
+
+def createIdentifier(data):
+  h = hashlib.new('sha1')
+  h.update(data.encode())
+  return h.hexdigest()
+
+identifier = ""
+
+try:
+  with open('id', 'r') as f:
+    identifier = f.read()
+except:
+  with open('id', 'w') as f:
+    data_for_encoding = "{}-{}".format( platform.platform(), random.randrange(1000000, 9999999) )
+    identifier = createIdentifier( data_for_encoding )
+    f.write(identifier)
 
 class Config():
-    
+
+  IDENTIFIER = identifier
+
   APP_NAME = 'Screenshot Matcher'
   ICON_PATH = 'gui/icon.png'
   HOST = common.utils.getCurrentIPAddress()
