4b4f51fcf91c199dff7ba7a3375c2e89b5557784
Alex <alex_ha@live.de>
2020-06-06 08:51:00 +0200

    rearrange tray menu

--- screenshotmatcher/gui/tray.py
+++ screenshotmatcher/gui/tray.py
@@ -20,6 +20,13 @@ class Tray():
         icon=self.get_icon(),
         menu=menu(
             item(
+                'Show QR code',
+                lambda icon: self.onclick_qr()),
+            item(
+                'Open Results Dir',
+                lambda icon: self.onclick_results()),
+            menu.SEPARATOR,
+            item(
                 'Matching Algorithm',
                 lambda icon: None,
                 enabled=False),
@@ -33,13 +40,6 @@ class Tray():
                 self.set_state('ORB'),
                 checked=self.get_state('ORB'),
                 radio=True),
-            menu.SEPARATOR,
-            item(
-                'Show QR code',
-                lambda icon: self.onclick_qr()),
-            item(
-                'Open Results Dir',
-                lambda icon: self.onclick_results()),
             menu.SEPARATOR,
             item(
                 'Quit',
