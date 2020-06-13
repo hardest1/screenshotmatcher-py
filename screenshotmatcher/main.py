2c2902a60842efd5232e31ae4b58203751da0bc9
Alex <alex_ha@live.de>
2020-06-13 18:23:10 +0200

    pack main script in main func

--- screenshotmatcher/main.py
+++ screenshotmatcher/main.py
@@ -12,15 +12,18 @@ import common.utils
 from matching.matcher import Matcher
 from server.server import Server
 
-# Init Server
-server = Server()
+def main():
+  # Init Server
+  server = Server()
 
-# Init Tray
-tray = gui.tray.Tray(Config.APP_NAME)
+  # Init Tray
+  tray = gui.tray.Tray(Config.APP_NAME)
 
-# Start server in different thread
-x = threading.Thread(target=server.start, args=(), daemon=True)
-x.start()
+  # Start server in different thread
+  x = threading.Thread(target=server.start, args=(), daemon=True)
+  x.start()
 
-# Start Tray event loop
-tray.run()
+  # Start Tray event loop
+  tray.run()
+
+main()
